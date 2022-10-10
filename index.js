20e798e5e6264cbae452a7c4972339c0358dbd8c
mganeko <masashi.g@gmail.com>
2022-10-10 16:54:46 +0900

    message variable

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http = require('http');
 
+const message = 'Main Slot';
 const server = http.createServer(function (request, response) {
   response.writeHead(200, { "Content-Type": "text/html" });
-  response.end("<html><body><h1>Hello Tiny3 World!</h1></body></html>");
+  response.end(`<html><body><h1>Hello ${message}!</h1></body></html>`);
 });
 
 const port = process.env.PORT || 1337;
